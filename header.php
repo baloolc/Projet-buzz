38da73c58a40cfa2e7988a8186c7d6eee0d043a0
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-03-23 10:21:25 +0100

    Modified index into .php and i add header.php

---
+++ header.php
@@ -0,0 +1,43 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/indexstyle.css">
+    <title>CV de l'espace</title>
+</head>
+
+<body>
+    <!-- section header/top page/landing -->
+    <section class="top-page">
+        <!-- Le header -->
+        <header class="header">
+            <nav class="nav">
+                <!-- icone à gauche -->
+                <div class="buzz-nav">
+                    <a  href="index.html"> Buzz L'Eclair</a>
+                </div>
+                <!-- Listes icones à droite -->
+
+                <div class="menu" id="#items">
+                    <a href="personnage_main.html">Star Command</a>
+                    <a href="personnage_main.html">Toys Story</a>
+                    <a href="personnage_main.html">James Buzz</a>
+                    <a href="contact.html">Contact</a>
+                </div>
+                <!-- icone burger -->
+                <a class="burger" href="items">
+                <svg xmlns="http://www.w3.org/2000/svg" class= "burgerIcon" width="24" height="24" fill="currentColor" class="bi bi-list"
+                    viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                </svg>
+            </a>
+            </nav>
+        </header>
