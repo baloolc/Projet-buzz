164b85ea61d5ec70e1dc0623207aac648916dbb4
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-03-23 14:24:54 +0100

    Finish the first step of the burger button

--- header.php
+++ header.php
@@ -11,6 +11,11 @@
                 <!-- Listes icones à droite -->
 
                 <div class="menu" id="items">
+                    <a class="burgerClose" href="items">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                        </svg>
+                    </a>
                     <a href="personnage_main.php">Star Command</a>
                     <a href="personnage_main.php">Toys Story</a>
                     <a href="personnage_main.php">James Buzz</a>
@@ -22,12 +27,7 @@
                         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                     </svg>
                 </a>
-                <a class="burgerClose" href="items">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                    </svg>
-                </a>
-                <a class="close" href="#"></a>
+                <!-- <a class="close" href="#"></a> -->
                 </a>
             </nav>
         </header>
