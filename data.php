05d7bd8fa5244565566d47e9d57fa9c1ec92d861
baloolc <viann45@hotmail.fr>
2022-03-23 16:23:31 +0100

    ajout de data et style page perso + responsive

--- data.php
+++ data.php
@@ -6,30 +6,22 @@ $data = [
             'bloc'  => 'bloc-one',
             'image' => 'bloc-one-img',
             'body'  => 'bloc-one-body',
-            'title' => 'Je suis très fort',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat mi et sapien 
-            tincidunt mollis. Cras magna risus, venenatis a ornare in, faucibus quis orci. 
-            sollicitudin placerat ipsum. Vivamus ac quam ut nibh pharetra aliquet. 
-            Mauris sed lorem vel mauris auctor convallis.',
+            'title' => 'Parlons de moi!',
+            'description' => 'Je suis Buzz l\'éclair, vous allez me dire pourquoi l\'éclair? Et bien je vous répondrais que je suis rapide comme un éclaire. Je suis considéré comme le meilleur agent de Start Command, en effet j\'ai été amené à exécuter diverse missions dans la galaxie en solo ou ien avec ma partenaire Mira Nova, mais elle sans intérêt, concentrez-vous sur moi!'
         ],
         [
             'bloc'  => 'bloc-two',
             'image' => 'bloc-two-img',
             'body'  => 'bloc-two-body',
-            'title' => 'Je suis le meilleur ',
-            'description' => 'Cras magna risus, venenatis a ornare in, faucibus quis orci. 
-            sollicitudin placerat ipsum. Vivamus ac quam ut nibh pharetra aliquet. 
-            Mauris sed lorem vel mauris auctor convallis.',
+            'title' => 'Les espèces rencontrées ',
+            'description' => 'Dans mes diverses missions intergalactique, j\'ai rencontré diverse espèces extraterrestre. J\'ai pu établir le contact et faire des alliances pour explulser les hordes Zurgienne de ses galaxies. Mes talents de négociateur étaient essensiel. Vers l\'infini et au delà!',
         ],
         [
             'bloc'  => 'bloc-three',
             'image' => 'bloc-three-img',
             'body'  => 'bloc-three-body',
-            'title' => 'Je suis le roi du monde ',
-            'description' => 'Vivamus ac quam ut nibh pharetra aliquet. 
-            Mauris sed lorem vel mauris auctor convallis. Cras magna risus, venenatis a ornare in, faucibus quis orci. 
-            sollicitudin placerat ipsum. Vivamus ac quam ut nibh pharetra aliquet. 
-            Mauris sed lorem vel mauris auctor convallis.',
+            'title' => 'La guerre contre l\'infâme Empereur Zurg ',
+            'description' => 'Dans notre exploration, la terre c\'est fait un ennemi juré, l\'infâme empereur Zurg. J\'ai passé des années à houster ses sbires des galacties, jusqu\'à ce que je le localise dans son repère. Après deux heures de combat intense, j\'étais sur le point de le mettre hors d\'état de nuire mais il réussi à s\'en fuir et je n\'ai plus entendu parler de lui.',
         ],
     ],
     ];
