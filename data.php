4a17edbc3c5209952fbdc883228c177b32178178
Ysedda <arthur.baudoin@icloud.com>
2022-03-23 14:26:16 +0100

    more data

--- data.php
+++ data.php
@@ -31,4 +31,65 @@ $starcommand = [
             Mauris sed lorem vel mauris auctor convallis.',
         ],
     ];
-    
+$toystory = [
+        [
+            'bloc'  => 'bloc-one',
+            'image' => 'bloc-one-img',
+            'body'  => 'bloc-one-body',
+            'title' => 'Je suis très fort',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat mi et sapien 
+            tincidunt mollis. Cras magna risus, venenatis a ornare in, faucibus quis orci. 
+            sollicitudin placerat ipsum. Vivamus ac quam ut nibh pharetra aliquet. 
+            Mauris sed lorem vel mauris auctor convallis.',
+        ],
+        [
+            'bloc'  => 'bloc-two',
+            'image' => 'bloc-two-img',
+            'body'  => 'bloc-two-body',
+            'title' => 'Je suis le meilleur ',
+            'description' => 'Cras magna risus, venenatis a ornare in, faucibus quis orci. 
+            sollicitudin placerat ipsum. Vivamus ac quam ut nibh pharetra aliquet. 
+            Mauris sed lorem vel mauris auctor convallis.',
+        ],
+        [
+            'bloc'  => 'bloc-three',
+            'image' => 'bloc-three-img',
+            'body'  => 'bloc-three-body',
+            'title' => 'Je suis le roi du monde ',
+            'description' => 'Vivamus ac quam ut nibh pharetra aliquet. 
+            Mauris sed lorem vel mauris auctor convallis. Cras magna risus, venenatis a ornare in, faucibus quis orci. 
+            sollicitudin placerat ipsum. Vivamus ac quam ut nibh pharetra aliquet. 
+            Mauris sed lorem vel mauris auctor convallis.',
+        ],
+    ];    
+$jamesbuzz = [
+        [
+            'bloc'  => 'bloc-one',
+            'image' => 'bloc-one-img',
+            'body'  => 'bloc-one-body',
+            'title' => 'Je suis très fort',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat mi et sapien 
+            tincidunt mollis. Cras magna risus, venenatis a ornare in, faucibus quis orci. 
+            sollicitudin placerat ipsum. Vivamus ac quam ut nibh pharetra aliquet. 
+            Mauris sed lorem vel mauris auctor convallis.',
+        ],
+        [
+            'bloc'  => 'bloc-two',
+            'image' => 'bloc-two-img',
+            'body'  => 'bloc-two-body',
+            'title' => 'Je suis le meilleur ',
+            'description' => 'Cras magna risus, venenatis a ornare in, faucibus quis orci. 
+            sollicitudin placerat ipsum. Vivamus ac quam ut nibh pharetra aliquet. 
+            Mauris sed lorem vel mauris auctor convallis.',
+        ],
+        [
+            'bloc'  => 'bloc-three',
+            'image' => 'bloc-three-img',
+            'body'  => 'bloc-three-body',
+            'title' => 'Je suis le roi du monde ',
+            'description' => 'Vivamus ac quam ut nibh pharetra aliquet. 
+            Mauris sed lorem vel mauris auctor convallis. Cras magna risus, venenatis a ornare in, faucibus quis orci. 
+            sollicitudin placerat ipsum. Vivamus ac quam ut nibh pharetra aliquet. 
+            Mauris sed lorem vel mauris auctor convallis.',
+        ],
+    ];
